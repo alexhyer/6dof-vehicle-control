01d275074a40cbc9b8b7aab79e216858c40b7207
Alex Hyer <35442826+alexhyer@users.noreply.github.com>
2022-09-08 14:34:33 -0500

    Add files via upload

---
+++ lib/gnss.cpp
@@ -0,0 +1,52 @@
+#include "Arduino.h"
+#include "gnss.h"
+
+//Credit for 10hz ublox gnss code goes to iforce2d and can be found here: https://youtu.be/TwhCX0c8Xe0
+
+void Gnss::calcChecksum(unsigned char* CK) {
+  memset(CK, 0, 2);
+  for (int i = 0; i < (int)sizeof(NAV_POSLLH); i++) {
+    CK[0] += ((unsigned char*)(&posllh))[i];
+    CK[1] += CK[0];
+  }
+}
+
+bool Gnss::processGPS() {
+  static int fpos = 0;
+  static unsigned char checksum[2];
+  const int payloadSize = sizeof(NAV_POSLLH);
+
+  while ( Serial3.available() ) {
+    byte c = Serial3.read();
+    if ( fpos < 2 ) {
+      if ( c == UBX_HEADER[fpos] )
+        fpos++;
+      else
+        fpos = 0;
+    }
+    else {
+      if ( (fpos - 2) < payloadSize )
+        ((unsigned char*)(&posllh))[fpos - 2] = c;
+
+      fpos++;
+
+      if ( fpos == (payloadSize + 2) ) {
+        calcChecksum(checksum);
+      }
+      else if ( fpos == (payloadSize + 3) ) {
+        if ( c != checksum[0] )
+          fpos = 0;
+      }
+      else if ( fpos == (payloadSize + 4) ) {
+        fpos = 0;
+        if ( c == checksum[1] ) {
+          return true;
+        }
+      }
+      else if ( fpos > (payloadSize + 4) ) {
+        fpos = 0;
+      }
+    }
+  }
+  return false;
+}
